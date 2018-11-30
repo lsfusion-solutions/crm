fd06f1c3865f2b5154a63b89783291a9f5d94867
Aliaksei Kirkouski <40161740+CrushBy@users.noreply.github.com>
2018-11-30 10:51:23 +0300

    Add files via upload

---
+++ src/main/java/lsfusion/crm/region/by/masterdata/ImportNBRBExchangeRateActionProperty.java
@@ -0,0 +1,151 @@
+package lsfusion.crm.region.by.masterdata;
+
+import lsfusion.crm.integration.DefaultIntegrationActionProperty;
+import lsfusion.server.classes.ConcreteCustomClass;
+import lsfusion.server.classes.DateClass;
+import lsfusion.server.classes.ValueClass;
+import lsfusion.server.data.SQLHandledException;
+import lsfusion.server.integration.*;
+import lsfusion.server.logics.property.ClassPropertyInterface;
+import lsfusion.server.logics.property.ExecutionContext;
+import lsfusion.server.logics.scripted.ScriptingErrorLog;
+import lsfusion.server.logics.scripted.ScriptingLogicsModule;
+import lsfusion.server.session.DataSession;
+import org.apache.commons.lang3.time.DateUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ImportNBRBExchangeRateActionProperty extends DefaultIntegrationActionProperty {
+
+    public ImportNBRBExchangeRateActionProperty(ScriptingLogicsModule LM, ValueClass valueClass) {
+        super(LM, valueClass);
+    }
+
+    public ImportNBRBExchangeRateActionProperty(ScriptingLogicsModule LM, ValueClass... classes) {
+        super(LM, classes);
+    }
+
+    @Override
+    public void executeCustom(ExecutionContext<ClassPropertyInterface> context) throws SQLException, SQLHandledException {
+    }
+
+    protected void importExchanges(Date dateFrom, Date dateTo, String shortNameCurrency, ExecutionContext context, boolean denominate) throws ScriptingErrorLog.SemanticErrorException, IOException, SQLException, ParseException, SQLHandledException, JSONException {
+
+
+        List<Exchange> exchangesList = importExchangesFromXMLDenominated(dateFrom, dateTo, shortNameCurrency, denominate);
+
+        if (exchangesList != null) {
+
+            ImportField typeExchangeBYRField = new ImportField(findProperty("name[TypeExchange]"));
+            ImportField typeExchangeForeignField = new ImportField(findProperty("name[TypeExchange]"));
+            ImportField currencyField = new ImportField(findProperty("shortName[Currency]"));
+            ImportField homeCurrencyField = new ImportField(findProperty("shortName[Currency]"));
+            ImportField rateField = new ImportField(findProperty("rate[TypeExchange,Currency,DATE]"));
+            ImportField foreignRateField = new ImportField(findProperty("rate[TypeExchange,Currency,DATE]"));
+            ImportField dateField = new ImportField(DateClass.instance);
+
+            ImportKey<?> typeExchangeBYRKey = new ImportKey((ConcreteCustomClass) findClass("TypeExchange"),
+                    findProperty("typeExchange[VARISTRING[50]]").getMapping(typeExchangeBYRField));
+
+            ImportKey<?> typeExchangeForeignKey = new ImportKey((ConcreteCustomClass) findClass("TypeExchange"),
+                    findProperty("typeExchange[VARISTRING[50]]").getMapping(typeExchangeForeignField));
+
+            ImportKey<?> currencyKey = new ImportKey((ConcreteCustomClass) findClass("Currency"),
+                    findProperty("currencyShortName[STRING[3]]").getMapping(currencyField));
+
+            ImportKey<?> homeCurrencyKey = new ImportKey((ConcreteCustomClass) findClass("Currency"),
+                    findProperty("currencyShortName[STRING[3]]").getMapping(homeCurrencyField));
+
+            List<ImportProperty<?>> props = new ArrayList<>();
+
+            props.add(new ImportProperty(typeExchangeBYRField, findProperty("name[TypeExchange]").getMapping(typeExchangeBYRKey)));
+            props.add(new ImportProperty(homeCurrencyField, findProperty("currency[TypeExchange]").getMapping(typeExchangeBYRKey),
+                    object(findClass("Currency")).getMapping(homeCurrencyKey)));
+            props.add(new ImportProperty(rateField, findProperty("rate[TypeExchange,Currency,DATE]").getMapping(typeExchangeBYRKey, currencyKey, dateField)));
+
+            props.add(new ImportProperty(typeExchangeForeignField, findProperty("name[TypeExchange]").getMapping(typeExchangeForeignKey)));
+            props.add(new ImportProperty(currencyField, findProperty("currency[TypeExchange]").getMapping(typeExchangeForeignKey),
+                    object(findClass("Currency")).getMapping(currencyKey)));
+            props.add(new ImportProperty(foreignRateField, findProperty("rate[TypeExchange,Currency,DATE]").getMapping(typeExchangeForeignKey, homeCurrencyKey, dateField)));
+
+            List<List<Object>> data = new ArrayList<>();
+            for (Exchange e : exchangesList) {
+                data.add(Arrays.asList((Object) "НБРБ (BYR)", "НБРБ (" + e.currencyID + ")", e.currencyID, e.homeCurrencyID, e.exchangeRate, new BigDecimal(1 / e.exchangeRate.doubleValue()), e.date));
+            }
+            ImportTable table = new ImportTable(Arrays.asList(typeExchangeBYRField, typeExchangeForeignField, currencyField,
+                    homeCurrencyField, rateField, foreignRateField, dateField), data);
+
+            DataSession session = context.getSession();
+            IntegrationService service = new IntegrationService(session, table, Arrays.asList(typeExchangeBYRKey,
+                    typeExchangeForeignKey, currencyKey, homeCurrencyKey), props);
+            service.synchronize(true, false);
+            //session.apply(LM.getBL());
+        }
+    }
+
+    private List<Exchange> importExchangesFromXMLDenominated(Date dateFrom, Date dateTo, String shortNameCurrency, boolean denominate) throws IOException, ParseException, JSONException {
+
+        List<Exchange> exchangesList = new ArrayList<>();
+
+        JSONArray document = readJsonFromUrl("http://www.nbrb.by/API/ExRates/Currencies");
+        for (int i = 0; i < document.length(); i++) {
+            JSONObject jsonObject = document.getJSONObject(i);
+
+            String charCode = jsonObject.getString("Cur_Abbreviation");
+
+            if (shortNameCurrency.equals(charCode)) {
+                String id = jsonObject.getString("Cur_ID");
+                BigDecimal scale = new BigDecimal(jsonObject.getString("Cur_Scale"));
+
+                JSONArray exchangeDocument = readJsonFromUrl("http://www.nbrb.by/API/ExRates/Rates/Dynamics/" + id
+                        + "?startDate=" + new SimpleDateFormat("MM/dd/yyyy").format(dateFrom)
+                        + "&endDate=" + new SimpleDateFormat("MM/dd/yyyy").format(dateTo));
+
+                for (int j = 0; j < exchangeDocument.length(); j++) {
+
+                    JSONObject exchangeNode = exchangeDocument.getJSONObject(j);
+
+                    BigDecimal rate = new BigDecimal(exchangeNode.getString("Cur_OfficialRate"));
+                    if(denominate)
+                        rate = safeDivide(rate, 10000);
+
+                    exchangesList.add(new Exchange(charCode, "BYN", new Date(DateUtils.parseDate(exchangeNode.getString("Date"), new String[]{"yyyy-MM-dd'T'HH:mm:ss"}).getTime()),
+                            safeDivide(rate, scale, 6)));
+                }
+                if (exchangesList.size() > 0)
+                    return exchangesList;
+            }
+        }
+        return exchangesList;
+    }
+
+    public JSONArray readJsonFromUrl(String url) throws IOException, JSONException {
+        try (InputStream is = new URL(url).openStream()) {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            return new JSONArray(jsonText);
+        }
+    }
+
+    private String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+}
---
+++ src/main/java/lsfusion/crm/region/by/masterdata/ImportNBRBExchangeRateLastDaysActionProperty.java
@@ -0,0 +1,59 @@
+package lsfusion.crm.region.by.masterdata;
+
+import com.google.common.base.Throwables;
+import lsfusion.server.classes.ValueClass;
+import lsfusion.server.data.SQLHandledException;
+import lsfusion.server.logics.DataObject;
+import lsfusion.server.logics.property.ClassPropertyInterface;
+import lsfusion.server.logics.property.ExecutionContext;
+import lsfusion.server.logics.scripted.ScriptingErrorLog;
+import lsfusion.server.logics.scripted.ScriptingLogicsModule;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Iterator;
+
+public class ImportNBRBExchangeRateLastDaysActionProperty extends ImportNBRBExchangeRateActionProperty {
+    private final ClassPropertyInterface currencyInterface;
+
+    public ImportNBRBExchangeRateLastDaysActionProperty(ScriptingLogicsModule LM, ValueClass... classes) {
+        super(LM, classes);
+
+        Iterator<ClassPropertyInterface> i = interfaces.iterator();
+        currencyInterface = i.next();
+    }
+
+    @Override
+    public void executeCustom(ExecutionContext<ClassPropertyInterface> context) throws SQLException, SQLHandledException {
+
+        try {
+
+            DataObject currencyObject = context.getDataKeyValue(currencyInterface);
+
+            String shortNameCurrency = (String) findProperty("shortName[Currency]").read(context, currencyObject);
+            long currentTime = Calendar.getInstance().getTimeInMillis();
+            Integer days = (Integer) findProperty("importNBRBExchangeRateDaysCount[]").read(context);
+            if (shortNameCurrency != null && days != null && days > 0) {
+                Date separationDate = new Date(2016 - 1900, 5, 30); //30.06.2016
+                Date denominationDate = new Date(2016 - 1900, 6, 1); //01.07.2016
+                Date dateFrom = new Date(currentTime - (long) days * 24 * 3600 * 1000);
+                Date dateTo = new Date(currentTime + (long) 24 * 3600 * 1000);
+                if(dateFrom.getTime() <= separationDate.getTime() && dateTo.getTime() > separationDate.getTime()) {
+                    importExchanges(dateFrom, separationDate, shortNameCurrency, context, true);
+                    importExchanges(denominationDate, dateTo, shortNameCurrency, context, false);
+                } else {
+                    importExchanges(dateFrom, dateTo, shortNameCurrency, context, dateFrom.getTime() < separationDate.getTime());
+                }
+
+            }
+
+        } catch (IOException | ScriptingErrorLog.SemanticErrorException | ParseException | JSONException e) {
+            throw Throwables.propagate(e);
+        }
+
+    }
+}
